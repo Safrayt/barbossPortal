4bb999078686e5088f8263bd12b94af8bbfa979e
Safrayt <WorkOutLands@mail.ru>
2024-10-16 13:28:37 +0700

    update

--- script.js
+++ script.js
@@ -12,12 +12,16 @@
     let mainContainerWidthPosition = 1
 
 
-function containersSizes() {
+function containersAndScreensSizes() {
 // размеры главного контейнера 
     // высота
         mainContainerHeight = window.innerHeight;
     // ширина
-       mainContainerWidth = window.innerHeight * 0.5625;
+    if (window.innerWidth/window.innerHeight < 0.5625) {
+        mainContainerWidth = window.innerWidth;
+    } else {
+        mainContainerWidth = window.innerHeight * 0.5625;
+        }
     // размеры для стиля главного контейнера
     document.querySelector('.main-container').style.height = mainContainerHeight + 'px'
     document.querySelector('.main-container').style.width = mainContainerWidth + 'px'
@@ -27,21 +31,38 @@ function containersSizes() {
         slaveContainerHeight = window.innerHeight;
     // ширина
         // ширина рабского контейрена зависит от переменной позиции рабского контейнера
-        slaveContainerWidth = window.innerHeight * 0.5625 * mainContainerWidthPosition;
+        if (window.innerWidth/window.innerHeight < 0.5625) {
+            slaveContainerWidth = window.innerWidth * mainContainerWidthPosition;
+        } else {
+            slaveContainerWidth = window.innerHeight * 0.5625 * mainContainerWidthPosition;
+            }
     // размеры для стиля рабского контейнера
     document.querySelector('.slave-container').style.height = slaveContainerHeight + 'px'
     document.querySelector('.slave-container').style.width = slaveContainerWidth + 'px'
+
+// размеры всех экранов
+    document.querySelector('.screen').style.height = window.innerHeight + 'px'
+    document.querySelector('.screen').style.width = window.innerHeight * 0.5625 + 'px'
+
+// размеры сетки на экранах
+    // сетка для #main-screen
+    document.querySelector('#main-screen').style.gridTemplateRows = mainContainerHeight*0.15 + 'px ' + mainContainerHeight*0.35 + 'px ' + mainContainerHeight*0.5 + 'px ';
+    
 }
 
 function containersResize() {
     window.addEventListener('resize', function() {
     console.log("Размер окна изменен");
-    containersSizes()
+    containersAndScreensSizes()
   });
   }
 
-  containersSizes()
+  containersAndScreensSizes()
   containersResize()
+
+
+
+
 
 
 // Кнопки
@@ -49,12 +70,12 @@ function containersResize() {
         // Кнопка перехода на следующий экран
         function navButtonForward() {
             mainContainerWidthPosition = mainContainerWidthPosition + 1
-            containersSizes()
+            containersAndScreensSizes()
         }
         // Кнопка перехода на предыдущий экран
         function navButtonBack() {
             if (mainContainerWidthPosition > 1) {
                 mainContainerWidthPosition = mainContainerWidthPosition - 1
-                containersSizes()
+                containersAndScreensSizes()
             }
         }
